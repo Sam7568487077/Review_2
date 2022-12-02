2fa9af7c635f879dfbe0dcd7c51d7c3cb416b1b8
Sam K Varghese <ksam071@gmail.com>
2022-12-02 19:30:19 +0530

    Second commit

--- Flip_coin.sh
+++ Flip_coin.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 echo "Flip Coin Combination Problem"
+flip=$(( RANDOM%2));
+if [ $flip == 0 ];then
+echo "The coin has flipped and the result is head"
+else
+echo "The coin has flipped and the result is tail"
+fi
